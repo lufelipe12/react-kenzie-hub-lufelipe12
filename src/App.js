c7dfe0ceb16e35122f483141c6adde3106d2bed3
Luiz Felipe Dias <lufelipe.dias@gmail.com>
2022-02-22 20:17:08 -0300

    Feature: Add rca, libs, css variables and structures.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import GlobalStyles from "./GlobalStyles";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyles />
+      div
+    </div>
+  );
+}
+
+export default App;
